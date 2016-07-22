6e4563859246511613c6bd819a0e4faed9ef6f8d
Nazar <kryvyynazariy@gmail.com>
2016-07-22 15:06:29 +0300

    first push

--- src/js/develop/develop_6.js
+++ src/js/develop/develop_6.js
@@ -1,8 +1,44 @@
+function sectionHeight(){
+	var windowHeight = $(window).height();
+	
+	var footerHeight = $(".footer").height();
+	
+	var artboardHeight = windowHeight - footerHeight;
+	
+	$('.artboard-main').css('height', artboardHeight);
+}
 
+function showPosts(){
+	var countPosts = $(".post");
+	var i = 0;
+	console.log(countPosts);
+
+	console.log($(countPosts).eq(0));
+
+	var messagesAdding = setInterval(function(){
+		$('.row').eq(i).addClass('active');
+		i++;
+		if(i == $('.row').length){
+			$(".printing-process").addClass("hide-inner-text");
+			setTimeout(function(){
+				$(".submit-button").addClass('show');
+			},1000)
+		}
+		if(i==$('.row').length){
+			clearInterval(messagesAdding);
+		}
+	}, 3000);	
+}
 
 
 $(document).ready(function(){
+	sectionHeight();
 
+	$(function(){
+		$('.scroll-pane').jScrollPane();
+	});
+
+	showPosts();
 });
 
 $(window).load(function(){
